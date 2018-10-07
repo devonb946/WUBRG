07ab88d6d93620e407712416d1757312d93e213b
devon <devonbrown946@gmail.com>
2018-10-06 21:00:26 -0400

    Set up static root to serve static files in production

--- wubrgweb/settings.py
+++ wubrgweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ls5fl)!1v*prq4v43v&9%w7gezw02ozd5w&sfj@vbbqxe4to(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/wubrg-mtg.herokuapp.com/static/"
 
 # Login/Logout URLS
 
