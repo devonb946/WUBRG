0b21f2e956d48672d40da6e7a7477f7926cddff8
SchmidtNikolas <schmid17@duq.edu>
2018-10-14 17:04:20 -0400

    First steps towards PostgreSQL Migration

--- browse/models.py
+++ browse/models.py
@@ -1,30 +1,8 @@
+import uuid
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 class Card(models.Model):
-    name = models.CharField(max_length=150)
-    set = models.CharField(max_length=4)
-    collector_number = models.CharField(max_length=75)
-
-    def __str__(self):
-        return self.name
-
-class image_uris(models.Model):
-    small = models.URLField()
-    normal = models.URLField()
-    large = models.URLField()
-    png = models.URLField()
-    art_crop = models.URLField()
-    border_crop = models.URLField()
-
-    # TODO: Make sure everything works with array-like entries
-    #colors
-    #color_identity
-    #color_indicator
-    #legalities
-    #image_uris
-
-    # TODO: Make sure everything works for cards with unuasual faces/layouts
-    #multiverse_ids
-    #all_parts
-    #card_faces
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False)
+    data = JSONField(null=False)
--- wubrgweb/settings.py
+++ wubrgweb/settings.py
@@ -82,11 +82,12 @@ WSGI_APPLICATION = 'wubrgweb.wsgi.application'
 #TODO: obfuscate username/pw values
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'heroku_v5994ldl',
-        'USER': 'wubrgadmin',
-        'PASSWORD': 'hunter2',
-        'HOST': 'mongodb://wubrgadmin:hunter2@ds227053-a0.mlab.com:27053,ds227053-a1.mlab.com:27053/heroku_v5994ldl?replicaSet=rs-ds227053',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6tt07mv68p5e3',
+        'USER': 'yolvyjabqqcslj',
+        'PASSWORD': 'f9956ff96e2f12f0c0cda3d0bf9a1e0af88820a88ddda88258310d213ab35634',
+        'HOST': 'ec2-50-17-225-140.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
