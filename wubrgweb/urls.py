bc7727b650d07bea088e2d1313e62a482638af76
Devon Brown <devonbrown@Devons-MacBook-Pro.local>
2018-10-14 20:42:42 -0400

    Fix template path

--- wubrgweb/urls.py
+++ wubrgweb/urls.py
@@ -35,7 +35,5 @@ urlpatterns = [
 ]
 
 # Serve static files for development
-# if (os.environ.get('WUBRG_SERVE_STATIC')):
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if (os.environ.get('WUBRG_SERVE_STATIC')):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
