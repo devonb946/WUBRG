5d0e9406a4e9af1553d38e06e4751ecc26f0fc83
Devon Brown <devonbrown@Devons-MacBook-Pro.local>
2018-10-14 20:30:18 -0400

    Add all allowed hosts

--- wubrgweb/settings.py
+++ wubrgweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('WUBRG_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('WUBRG_DEBUG')
 
-ALLOWED_HOSTS = ['wubrg-mtg.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'wubrg-mtg.herokuapp.com']
 
 
 # Application definition
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wubrgweb.urls'
@@ -128,7 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Login/Logout URLS
 
