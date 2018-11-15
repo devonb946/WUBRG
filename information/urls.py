401094b3e40ee8c0f2cfc985bdaa99bdeabd8bda
SchmidtNikolas <schmid17@duq.edu>
2018-11-15 18:50:26 -0500

    Streamlined RSS stuff.

--- information/urls.py
+++ information/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
-    # informational tools paths
-    path('mtgrss/', views.mtgrss, name='mtgrss'),
-    path('youtube/', views.youtube, name='youtube'),
 ]
--- information/views.py
+++ information/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from xml.sax import saxutils as su
+
 
 # informational tools views
 def index(request):
+
+    r = BeautifulSoup(su.unescape(requests.get('https://magic.wizards.com/en/rss/rss.xml').text))
+    for i in r:
+        print(i, "\n")
+    context = {
+
+    }
     return render(request, 'information/base.html')
 
 def mtgrss(request):
