cae79faf114d64571f5c91bf7f9abec142994b45
Devon Brown <devonbrown@Devons-MacBook-Pro.local>
2018-12-02 13:22:54 -0500

    Hide stuff for open sourcing

--- update_database.py
+++ update_database.py
@@ -1,10 +1,16 @@
 import psycopg2 as ps2
-import uuid, json, requests
+import os, uuid, json, requests
 from timeit import default_timer as timer
 
 start = timer()
 
-conn = ps2.connect(dbname="d6tt07mv68p5e3", user="yolvyjabqqcslj", password="f9956ff96e2f12f0c0cda3d0bf9a1e0af88820a88ddda88258310d213ab35634", host="ec2-50-17-225-140.compute-1.amazonaws.com", port=5432)
+conn = ps2.connect(
+    dbname = os.environ.get('WUBRG_NAME'),
+    user = os.environ.get('WUBRG_USER'),
+    password = os.environ.get('WUBRG_PASSWORD'),
+    host = os.environ.get('WUBRG_HOST'),
+    port = os.environ.get('WUBRG_PORT'),
+)
 
 
 url = "https://archive.scryfall.com/json/scryfall-default-cards.json"
